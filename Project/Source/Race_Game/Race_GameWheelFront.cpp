5e104ff739e353f49a77f0b0f2f35bbdf9283627
Fleetlord1 <Fleetlord1@users.noreply.github.com>
2021-03-08 20:01:34 +0300

    some works with physics

--- Project/Source/Race_Game/Race_GameWheelFront.cpp
+++ Project/Source/Race_Game/Race_GameWheelFront.cpp
@@ -9,7 +9,7 @@ URace_GameWheelFront::URace_GameWheelFront()
 	ShapeRadius = 45.f;
 	ShapeWidth = 10.0f;
 	bAffectedByHandbrake = false;
-	SteerAngle = 50.f;
+	SteerAngle = 45.f;
 }
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
